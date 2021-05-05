463ea636baf0179f6be214151e148c499fa260ce
Ruth Hinkle <ruth.e.hinkle@gmail.com>
2021-05-05 11:52:34 -0500

    add filters, variables for bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,44 +1,83 @@
 // SELECT FIELDS & DIVS
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-// Select input field
-var idSelect = d3.select("#selDataset");
+// FUNCTION #1 of 4
+function buildCharts(patientID) {
+    // READ & INTERPRET THE DATA
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-// select the user input field
-var idSelect = d3.select("#selDataset");
+    // Read in the JSON data
+    d3.json("samples.json").then((data => {
+        console.log(data)
 
-// select the demographic info div's ul list group
-var demographicsTable = d3.select("#sample-metadata");
+        //filter by patient ID
+        var samples = data.samples
+        console.log(samples[0])
+        var filteredSample = samples.filter(bacteriaInfo => bacteriaInfo.id === patientID)[0]
 
-// select the bar chart div
-var barChart = d3.select("#bar");
 
-// select the bubble chart div
-var bubbleChart = d3.select("bubble");
+        // BAR CHART - NOTE: do console logs to check
+        // Grab sample_values for the bar chart
+        var sample_values = filteredSample.sample_values
 
-// select the gauge chart div
-var gaugeChart = d3.select("gauge");
+        // Use otu_ids as the labels for bar chart
+        var otu_ids = filteredSample.otu_ids
 
-// READ & INTERPRET THE DATA
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // use otu_labels as the hovertext for bar chart
+        var otu_labels = filteredSample.otu_labels
 
-// Read in the JSON data
-d3.json("data/samples.json").then((data => {
-}))
-    // BAR CHART
-    // Grab sample_values for the bar chart
-    // Use otu_ids as the labels for bar chart
-    // use otu_labels as the hovertext for bar chart
+        // Filter for top 10 otu-ids
 
-    // BUBBLE CHART
-    // Use otu_ids for the x values
-    // Use sample_values for the y values
-    // Use sample_values for the marker size
-    // Use otu_ids for the marker colors
-    // Use otu_labels for the text values
+        // BUBBLE CHART
+        // Use otu_ids for the x values
+        // Use sample_values for the y values
+        // Use sample_values for the marker size
+        // Use otu_ids for the marker colors
+        // Use otu_labels for the text values
 
-// DISPLAY SAMPLE METADATA (1 person's demographic data)
+        // DISPLAY SAMPLE METADATA (1 person's demographic data)
 
-// DISPLAY KEY-VALUE PAIRS FROM METADATA JSON OBJECT ON PAGE
+        // DISPLAY KEY-VALUE PAIRS FROM METADATA JSON OBJECT ON PAGE
 
-// UPDATE ALL PLOTS ANY TIME A NEW SAMPLE IS SELECTED
+        // UPDATE ALL PLOTS ANY TIME A NEW SAMPLE IS SELECTED
+    }))
+
+
+};
+
+// FUNCTION #2 of 4
+function populateDemoInfo(patientID) {
+
+    var demographicInfoBox = d3.select("#sample-metadata");
+
+    d3.json("samples.json").then(data => {
+        console.log(data)
+    })
+}
+
+// FUNCTION #3 of 4
+function optionChanged(patientID) {
+    console.log(patientID);
+    buildCharts(patientID);
+    populateDemoInfo(patientID);
+}
+
+// FUNCTION #4 of 4
+function initDashboard() {
+    var dropdown = d3.select("#selDataset")
+    d3.json("samples.json").then(data => {
+        var patientIDs = data.names;
+        patientIDs.forEach(patientID => {
+            dropdown.append("option").text(patientID).property("value", patientID)
+        })
+        buildCharts(patientIDs[0]);
+        populateDemoInfo(patientIDs[0]);
+    });
+};
+
+initDashboard();
+
+
+
+
+
